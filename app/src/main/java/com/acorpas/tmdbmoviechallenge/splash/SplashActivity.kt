cc24a16deed88f0a9a6ea8f41b87dd3600fc02e0
Albano Corpas <albano.corpas@mango.com>
2020-04-17 11:43:43 +0200

    created splash activity with 2 second of delay

---
+++ app/src/main/java/com/acorpas/tmdbmoviechallenge/splash/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.acorpas.tmdbmoviechallenge.splash
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.acorpas.tmdbmoviechallenge.R
+import java.util.concurrent.TimeUnit
+
+/**
+ * @author Mango
+ * @version 1.0 2020-04-17
+ */
+
+class MainActivity : AppCompatActivity() {
+    private val delayHandler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        delayHandler.postDelayed({
+            navigateToMainScreen()
+        }, SPLASH_DELAY)
+    }
+
+    private fun navigateToMainScreen() {
+        finish()
+    }
+
+    companion object {
+        private val SPLASH_DELAY = TimeUnit.SECONDS.toMillis(2)
+    }
+}
