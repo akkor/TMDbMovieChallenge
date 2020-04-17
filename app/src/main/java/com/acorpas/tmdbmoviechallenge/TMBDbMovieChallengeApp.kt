991be607545fe3cf5c4ce7bd649bc71d82bebfd3
Albano Corpas <albano.corpas@mango.com>
2020-04-17 12:45:55 +0200

    added application class

---
+++ app/src/main/java/com/acorpas/tmdbmoviechallenge/TMBDbMovieChallengeApp.kt
@@ -0,0 +1,12 @@
+package com.acorpas.tmdbmoviechallenge
+
+import android.app.Application
+
+/**
+ * @author Mango
+ * @version 1.0 2020-04-17
+ */
+
+class TMBDbMovieChallengeApp : Application() {
+
+}
--- app/src/main/java/com/acorpas/tmdbmoviechallenge/moviesList/MainActivity.kt
+++ app/src/main/java/com/acorpas/tmdbmoviechallenge/moviesList/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.acorpas.tmdbmoviechallenge
+package com.acorpas.tmdbmoviechallenge.moviesList
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.acorpas.tmdbmoviechallenge.R
 
 class MainActivity : AppCompatActivity() {
 
